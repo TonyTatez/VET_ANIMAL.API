a157b11fc76e301420efa6bf0b4c1b0e70a5057f
Stephano <stephanochang21@gmail.com>
2023-11-20 11:28:45 -0500

    Filter convert DatetimeNow to UTC Format

---
+++ ProyectoBaseNetCore/Filters/ConvertDatesFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+namespace VET_ANIMAL_API.Filters
+{
+    public class ConvertDatesFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (var parameter in context.ActionDescriptor.Parameters)
+            {
+                if (context.ActionArguments.TryGetValue(parameter.Name, out var argumentValue))
+                {
+                    if (argumentValue is DateTime dateTime)
+                    {
+                        // Convierte el DateTime a UTC antes de que llegue al controlador
+                        DateTime utcDateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+
+                        // Actualiza el valor del parámetro
+                        context.ActionArguments[parameter.Name] = utcDateTime;
+                    }
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Este método se ejecuta después de que se ha completado la ejecución de la acción.
+        }
+    }
+}
